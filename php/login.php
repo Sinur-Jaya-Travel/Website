a8a7e9edfbc3cd552fb90edc114b23442b7d88ed
dduuddeekk <jajajablablabla123@gmail.com>
2023-06-04 00:28:14 +0800

    Tambah Login Page dan menggunakan sha256.

---
+++ php/login.php
@@ -0,0 +1,27 @@
+<?php
+require '../vendor/autoload.php';
+use MongoDB\Client;
+
+$client = new MongoDB\Client;
+$database = $client->sinurJayaTravel;
+$collection = $database->selectCollection("penumpang");
+
+if (isset($_GET["username"]) && isset($_GET["password"])) {
+    $username = $_GET["username"];
+    $password = $_GET["password"];
+    $hash = hash("sha256", $password);
+
+    $query = [
+        "username" => $username,
+        "password" => $hash
+    ];
+
+    $result = $collection->findOne($query);
+
+    if ($result) {
+        echo "Login successful!";
+    } else {
+        echo "Invalid username or password.";
+    }
+}
+?>
