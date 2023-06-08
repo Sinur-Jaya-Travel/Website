1ad29cef796fa06eadb28807c2cdbe8a8ecd974e
dduuddeekk <jajajablablabla123@gmail.com>
2023-06-08 22:42:23 +0800

    Memperbaiki login.html dan menambah user.php.

--- php/login.php
+++ php/login.php
@@ -1,66 +1,30 @@
 <?php
-require '../vendor/autoload.php';
-use MongoDB\Client;
+    require '../vendor/autoload.php';
+    use MongoDB\Client;
 
-$client = new MongoDB\Client;
-$database = $client->sinurJayaTravel;
-$collection = $database->selectCollection("users");
+    $client = new MongoDB\Client;
+    $database = $client->sinurJayaTravel;
+    $collection = $database->selectCollection("users");
 
-$result = null;
+    if (isset($_GET["username"]) && isset($_GET["password"])) {
+        $username = $_GET["username"];
+        $password = $_GET["password"];
+        $hash = hash("sha256", $password);
 
-if (isset($_GET["username"]) && isset($_GET["password"])) {
-    $username = $_GET["username"];
-    $password = $_GET["password"];
-    $hash = hash("sha256", $password);
+        $query = [
+            "username" => $username,
+            "password" => $hash
+        ];
 
-    $query = [
-        "username" => $username,
-        "password" => $hash
-    ];
+        $result = $collection->findOne($query);
 
-    $result = $collection->findOne($query);
-
-    if ($result) {
-        header("Location: ../html/userindex.html");
-        exit();
+        if ($result) {
+            header("Location: ../html/userindex.html?username=$username&password=$hash");
+            exit();
+        } else {
+            echo "Invalid username or password.";
+        }
     } else {
-        echo "Invalid username or password.";
+        echo "Username and password are required.";
     }
-}
-
-// $query = $collection->find();
-
-// require '../vendor/autoload.php';
-
-// use MongoDB\Client;
-
-// // Establishing connection to the MongoDB server
-// $client = new MongoDB\Client;
-// $database = $client->sinurJayaTravel;
-// $collection = $database->users;
-
-// if (isset($_GET["username"]) && isset($_GET["password"])) {
-//     $username = $_GET["username"];
-//     $password = $_GET["password"];
-//     $hashedPassword = hash("sha256", $password);
-
-//     $query = [
-//         "username" => $username,
-//         "password" => $hashedPassword
-//     ];
-
-//     // Retrieving a single matching document from the "users" collection
-//     $result = $collection->findOne($query);
-//     var_dump($result);
-
-//     if ($result) {
-//         include "../html/userindex.html";
-//         exit(); // Terminate the script execution after including the file
-//     }
-// }
-
-// Display error message for invalid username or password
-// echo "Invalid username or password.";
-
-
 ?>
---
+++ php/user.php
@@ -0,0 +1,60 @@
+<?php
+    require '../vendor/autoload.php';
+    use MongoDB\Client;
+    $client = new MongoDB\Client;
+
+    $database = $client->sinurJayaTravel;
+    $collection = $database->selectCollection("users");
+
+    $document = null;
+
+    if (isset($_GET["username"])) {
+        $username = $_GET["username"];
+    
+        $query = [
+            "username" => $username
+        ];
+    
+        $result = $collection->findOne($query);
+    
+        if ($result != null) {
+            $document = $result;
+        } else {
+            echo "Invalid username or password.";
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PROFIL</title>
+</head>
+<body>
+<h1>Profil</h1>
+    <?php if (isset($document)): ?>
+        <ul class="bus-list">
+            <li>
+                <span>ID: <?php echo $document["id"]; ?></span>
+                <span>Nama: <?php echo $document["name"]; ?></span>
+                <span>Username: <?php echo $document["username"]; ?></span>
+                <span>Alamat: <?php echo $document["address"]; ?></span>
+                <span>Nomor HP: <?php echo $document["number"]; ?></span>
+                <span>Email: <?php echo $document["email"]; ?></span>
+
+                <form method="POST" onsubmit="return confirm('Are you sure you want to delete this document?')">
+                    <input type="hidden" name="delete" value="<?php echo $document["id"]; ?>">
+                    <button type="submit">Hapus Akun</button>
+                </form>
+                <form action="../php/getuser.php" method="POST">
+                    <input type="hidden" name="id" value="<?php echo $document["id"]; ?>">
+                    <button type="submit">Perbarui</button>
+                </form>
+            </li>
+        </ul>
+    <?php endif; ?>
+</body>
+</html>
